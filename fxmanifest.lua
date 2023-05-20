6414063f76e312d9c66b1511e1bcd694e73775f1
Max <maximilian.wittfeld@tu-dortmund.de>
2023-05-20 20:23:41 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "vehdeleter"
+description "Deletes unoccupied vehicles with the use of statebags and game timers"
+author "tens0rfl0w"
+version "1.0.0"
+
+lua54 'yes'
+use_fxv2_oal 'yes'
+
+server_script 'server.lua'
+
+dependencies { '/onesync' }
---
+++ server.lua
@@ -0,0 +1,23 @@
+Config = {}
+Config.frequency = 5 * 1000 --Seconds between each check
+Config.threshold = 3600 * 1000 --Seconds a vehicle has to be unoccupied before it gets deleted
+
+CreateThread(function()
+    while true do
+        Wait(Config.frequency)
+        local timer = GetGameTimer()
+        for _, veh in pairs(GetAllVehicles()) do
+            local ent = Entity(veh)
+            if ent then
+                local driver = GetPedInVehicleSeat(veh, -1)
+                if driver ~= 0 and IsPedAPlayer(driver) then
+                    ent.state.deletionTimer = timer
+                elseif ent.state.deletionTimer then
+                    if timer - ent.state.deletionTimer > Config.threshold then
+                        DeleteEntity(ent)
+                    end
+                end
+            end
+        end
+    end
+end)
